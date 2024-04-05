e805cc9c6eb4f5cfca55592843419d01c62dfe5a
Utkarsh <vermau2k01@gmail.com>
2024-04-05 18:11:51 +0530

    Runnable Interface

--- Core/RunnableInterface.java
+++ Core/RunnableInterface.java
@@ -1,48 +1,60 @@
 public class RunnableInterface {
     public static void main(String[] args) {
 
-        Runnable r1 = new t1();
-        Runnable r2 = new t2();
 
-        var thread1 = new Thread(r1);
-        var thread2 = new Thread(r2);
 
-        r1.run();
-        r2.run();
+
+        // Runnable Interface with anonmous inner class
+
+        Runnable r3 = ()->{
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Hey There");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        };
+
+        var thread3 = new Thread(r3);
+
+
+        thread3.start();
 
     }
 }
 
 
-class t1 implements  Runnable
-{
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hii");
-            try {
-                Thread.sleep(2);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-        }
-    }
-}
-
-class t2 implements  Runnable
-{
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello");
-            try {
-                Thread.sleep(2);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-        }
-    }
-}
+//class t1 implements  Runnable
+//{
+//    @Override
+//    public void run() {
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Hii");
+//            try {
+//                Thread.sleep(2);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//        }
+//    }
+//}
+//
+//class t2 implements  Runnable
+//{
+//
+//    @Override
+//    public void run() {
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Hello");
+//            try {
+//                Thread.sleep(2);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//        }
+//    }
+//}
