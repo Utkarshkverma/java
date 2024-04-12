9b40493f68766a11288f093cc266238af9870768
Utkarsh <vermau2k01@gmail.com>
2024-04-12 15:40:47 +0530

    print

---
+++ Core/print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class print {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+     
+       
+        System.out.println(num);
+    }
+}           
